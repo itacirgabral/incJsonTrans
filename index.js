92380735b386b9af1959b6ca6d317260fb150b2a
itacirgabral <itacirgabral@@users.noreply.github.com>
2018-11-08 22:53:43 -0300

    fix heap out of memory

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const ijson = require('i-json')
 const parser = ijson.createParser((value, path) => {
   if (path.length === 2) {
-    buffer2token.push(path.join('/') + '\n')
+    buffer2token.push(path[1] + '\n')
   }
-  return value
-}, 2)
 
-let n = 0
+  // JavaScript heap out of memory
+  // do not return value
+  //
+  return undefined
+
+}, 2)
 
 const Transform = require('stream').Transform
 const transform = (data, _, done) => {
@@ -16,4 +19,4 @@ const transform = (data, _, done) => {
 
 const buffer2token = new Transform({transform})
 
-require('fs').createReadStream('funcionarios.json').pipe(buffer2token).pipe(process.stdout)
+  require('fs').createReadStream('funcionarios.json').pipe(buffer2token).pipe(process.stdout)
